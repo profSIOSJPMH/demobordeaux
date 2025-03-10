12be3b5398eda4f3951eadb7ec08c6942cc1cbde
profSIOSJPMH <85568080+profSIOSJPMH@users.noreply.github.com>
2025-03-10 15:18:41 +0100

    Update app_unsecure.js - correction injection sql

--- app_unsecure.js
+++ app_unsecure.js
@@ -28,8 +28,9 @@ db.connect((err) => {
 });
 
 app.get("/user", (req, res) => {
+  const email = req.query.email;
   db.query(
-    "SELECT * FROM users WHERE email = '" + req.query.email + "'",
+    "SELECT * FROM users WHERE email = ?", [email],
     (err, result) => {
       if (err) throw err;
       res.json(result);
@@ -43,7 +44,7 @@ app.post("/login", (req, res) => {
     return res.status(400).json({ error: "Email et mot de passe requis" });
   }
   db.query(
-    "SELECT * FROM users WHERE email = '" + email + "'",
+    "SELECT * FROM users WHERE email = ?", [email],
     (err, results) => {
       if (err || results.length === 0) {
         return res.status(401).json({ error: "Utilisateur non trouvé" });
